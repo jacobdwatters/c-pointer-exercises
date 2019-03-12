bcaf929391e4ace24a1cf787215aab68f2257a13
Coulten Davis <coulten.davis23@gmail.com>
2019-03-12 16:14:39 -0600

    Added array of pointers Updated monolithic array

--- array_of_pointers.c
+++ array_of_pointers.c
@@ -1,11 +1,39 @@
 //
 // Created by Ivo Georgiev on 2019-03-07.
 //
-#include <stdio.h>
 
-void run_array_of_pointers() {
+#include <stdio.h>
+#include <stdlib.h>
+
+struct str{
+    int a;
+    char c;
+};
+
+void run_array_of_pointers(){
     printf("Running array of pointers\n");
 
+    struct str w1;
+    struct str w2;
+    struct str w3;
+    struct str w4;
 
+    struct str **arr_p = (struct str**) calloc(4, sizeof(struct str*));
+    struct str s_arr[] = {w1, w2, w3, w4};
 
+    char character = 'a';
+
+    for(int i = 0; i < 4; i++)
+    {
+        arr_p[i] = &s_arr[i];
+        arr_p[i]->a = i;
+        arr_p[i]->c = character;
+        character++;
+    }
+
+    printf("Data in array: ");
+    for(int i = 0; i < 4; i++)
+    {
+       printf("%d\t%c\n", arr_p[i]->a, arr_p[i]->c);
+    }
 }
--- monolithic_array.c
+++ monolithic_array.c
@@ -14,19 +14,21 @@ void run_monolithic_array() {
     };
 
     struct str t;
-    struct str* tp = calloc(4, sizeof(t)); //the type of the array is now pointers of struct str
+    struct str* tp = calloc(4, sizeof(t));
 
-    char data = 'a';
+    char character = 'a';
 
     for(int i = 0; i < 4; i++)
     {
         tp[i].a = i;
-        tp[i].c = data;
-        data++;
+        tp[i].c = character;
+        character++;
     }
     printf("Data in array:\n");
     for(int i = 0; i < 4; i++)
     {
         printf("%d\t%c\n", tp[i].a, tp[i].c);
     }
+
+    free(tp);
 }
